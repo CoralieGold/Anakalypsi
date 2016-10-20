f6509f7c4fe44cbee1a48921463cff0a4a20b5aa
coraliegoldbaum <coralie.goldbaum@biomouv.com>
2016-10-20 21:34:23 +0200

    Chapters Summary V2

--- chapters.php
+++ chapters.php
@@ -6,20 +6,135 @@
 	<?php include_once("menu.php") ?>
 
 	<h1>Les chapitres</h1>
-	<ul id="summary">
-		<li><a href="chapter1.php"> </a></li>
-		<li><a href="chapter2.php"> </a></li>
-		<li><a href="chapter3.php"> </a></li>
-		<li><a href="chapter4.php"> </a></li>
-		<li><a href="chapter5.php"> </a></li>
-		<li><a href="chapter6.php"> </a></li>
-		<li><a href="chapter7.php"> </a></li>
-		<li><a href="chapter8.php"> </a></li>
-		<li><a href="chapter9.php"> </a></li>
-		<li><a href="chapter10.php"> </a></li>
-		<li><a href="chapter11.php"> </a></li>
-	</ul>
+	<div id="summary" class="row">
+		<a href="chapter1.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>01</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+
+		<a href="chapter2.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>02</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+
+		<a href="chapter3.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>03</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+		
+		<a href="chapter4.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>04</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+		
+		<a href="chapter5.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>05</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+		
+		<a href="chapter6.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>06</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+
+		<a href="chapter7.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>07</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+		
+		<a href="chapter8.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>08</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+		
+		<a href="chapter9.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>09</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+
+		<a href="chapter10.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>10</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+		
+		<a href="chapter11.php" class="chapter one twelfth">
+			<div class="chapter_content">
+				<div class="chapter_number">
+					Chapitre <span>11</span>
+				</div>
+				<div class="chapter_title">St Sulpice</div>
+			</div>
+			<div class="chapter_point"></div>
+		</a>
+	
 
 </div>
 
 <?php include_once("footer.php") ?>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		//$("div.chapter_content").hide();
+
+
+		$(".chapter_point").mouseover(function() {
+			$(this).siblings("div.chapter_content").show();
+		});
+
+		$(".chapter_point").mouseout(function() {
+			//$(this).siblings("div.chapter_content").hide();
+		});		
+
+	});
+	
+</script>
