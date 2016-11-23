052e4d08bc0a544e7145231046be57474c74c7ed
coraliegoldbaum <coralie.goldbaum@biomouv.com>
2016-11-23 10:34:29 +0100

    Secret chapter v1 + Chapter summary v5

---
+++ chapter_secret.php
@@ -0,0 +1,22 @@
+<?php $title = "Les chapitres" ?>
+<?php include_once("header.php") ?>
+
+<div class="main bg-img">
+	<div class="gold-border center">
+
+		<?php include_once("menu.php") ?>
+
+		<div class="row">
+			<div class="eleven twelfths skip-one">
+				<h1>Chapitre secret</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="eight twelfths skip-two">
+				<p class="white">Blabla</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include_once("footer.php") ?>
